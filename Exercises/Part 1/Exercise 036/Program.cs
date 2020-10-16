8580be9404944ae34c50a0d11b1a36b9d8d39b59
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-16 12:18:56 +0300

    16.10.2020

--- Exercises/Part 1/Exercise 036/Program.cs
+++ Exercises/Part 1/Exercise 036/Program.cs
@@ -8,6 +8,25 @@ namespace exercise_36
     {
       // Write your code here:
 
+           while (true) {
+
+            Console.WriteLine("Give a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+                if (number == 0) {
+                break;
+                }
+
+                if (number < 0) {
+                Console.WriteLine("That is negative!");
+                }
+
+                else {
+                Console.WriteLine(number * number);
+                }
+          
+           }
+
     }
   }
 }
