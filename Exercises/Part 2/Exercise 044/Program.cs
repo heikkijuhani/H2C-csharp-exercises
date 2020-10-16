716ab73845be4fe30994737b35ecce52ae76ccc6
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-16 13:24:07 +0300

    16.10.2020

--- Exercises/Part 2/Exercise 044/Program.cs
+++ Exercises/Part 2/Exercise 044/Program.cs
@@ -9,6 +9,24 @@ namespace exercise_44
 
       // Write your code here:
 
+        int firsNumber = Convert.ToInt32(Console.ReadLine());
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        if (firsNumber > secondNumber){
+            Console.WriteLine(firsNumber+" is grEater than "+secondNumber+".");
+            }
+        if (firsNumber < secondNumber){
+        Console.WriteLine(firsNumber+" is less than "+secondNumber+".");
+        }
+
+        if (firsNumber == secondNumber){
+        Console.WriteLine(firsNumber+" is equal to "+secondNumber+".");
+        }
+
+        
+      Console.ReadKey();      
+
+
     }
   }
 }
