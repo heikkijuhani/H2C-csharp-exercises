9db43fca168b1317eb0f4c47d581aff61f1b102e
heikkijuhani <57320479+heikkijuhani@users.noreply.github.com>
2020-10-16 10:15:47 +0300

    16.10.2020

--- Exercises/Part 1/Exercise 029/Program.cs
+++ Exercises/Part 1/Exercise 029/Program.cs
@@ -1,13 +1,35 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace exercise_29
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // Write your code here:
+        static void Main(string[] args)
+        {
+            // Write your code here:
+            
+            Console.WriteLine("Give the first number!");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give the second number!");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
 
+            if (firstNumber > secondNumber)
+            {
+                Console.WriteLine("The larger number is " + firstNumber + "!");
+            }
+            if (secondNumber > firstNumber)
+            {
+                Console.WriteLine("The larger number is " + secondNumber + "!");
+            }
+            if (firstNumber == secondNumber)
+            {
+                Console.WriteLine("They are equal!");
+            }
+
+        }
     }
-  }
 }
