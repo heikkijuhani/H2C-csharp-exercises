785d2f73186fb78a0ab7640733f921ae30d706dc
heikkijuhani <57320479+heikkijuhani@users.noreply.github.com>
2020-10-16 10:06:47 +0300

    16.10.2020

--- Exercises/Part 1/Exercise 014/Program.cs
+++ Exercises/Part 1/Exercise 014/Program.cs
@@ -1,14 +1,24 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 
 namespace exercise_14
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Give me the truth!");
-      // Write your code here:
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Give me the truth!");
+            // Write your code here:
+            string truth = Console.ReadLine();
+            bool booleanValue = System.Convert.ToBoolean(truth);
+            Console.WriteLine(booleanValue);
 
+        }
     }
-  }
 }
