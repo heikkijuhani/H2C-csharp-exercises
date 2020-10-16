5c9b485adacbb4a272429e1b068280bb6db7e464
heikkijuhani <57320479+heikkijuhani@users.noreply.github.com>
2020-10-16 09:58:57 +0300

    16.10.2020

--- Exercises/Part 1/Exercise 010/Program.cs
+++ Exercises/Part 1/Exercise 010/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
 
 namespace exercise_10
 {
@@ -7,7 +13,16 @@ namespace exercise_10
         public static void Main(string[] args)
         {
             // Write your code here:
-
+            Console.WriteLine("I will tell a story, but I need some information.\n"+
+                "Give a name for main character:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Give the character a profession:");
+            string profession = Console.ReadLine();
+            Console.WriteLine("Here is the story:\n"+
+                "Once upon a time there was a "+profession+" Data Scientist called "+name+"\n"+
+                "On her way to work, "+name+" often pondered what being "+profession+" meant to them.\n"+
+                "When you work as a "+profession+" you meet interesting people.\n"+
+                name +" enjoys their work as "+profession+", The end.");
         }
     }
 }
