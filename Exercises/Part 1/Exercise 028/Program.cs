03d2abc60cc73bd2338905f958cb04a2d1fa1deb
heikkijuhani <57320479+heikkijuhani@users.noreply.github.com>
2020-10-16 10:10:58 +0300

    16.10.2020

--- Exercises/Part 1/Exercise 028/Program.cs
+++ Exercises/Part 1/Exercise 028/Program.cs
@@ -1,13 +1,29 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace exercise_28
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // Write your code here:
+        static void Main(string[] args)
+        {
+            // Write your code here:
+            
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
 
+            if (age < 18)
+            {
+                Console.WriteLine("You're under age!");
+            } else
+            {
+                Console.WriteLine("You're an adult!");
+            }
+
+
+        }
     }
-  }
 }
