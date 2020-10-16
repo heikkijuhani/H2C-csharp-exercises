41e4e507ace303a6726d6891d58d9f022d5d7d32
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-16 18:26:16 +0300

    16.10.2020

--- Exercises/Part 2/Exercise 048/Program.cs
+++ Exercises/Part 2/Exercise 048/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.Tracing;
 
 namespace exercise_48
 {
@@ -8,7 +9,38 @@ namespace exercise_48
     {
 
       // Write your code here:
- 
+        Console.WriteLine("Give numbers:");
+        int sum = 0;
+        int i = 0;
+        int even = 0;
+        int odd = 0;
+
+      while (true) {
+
+        int number = Convert.ToInt32(Console.ReadLine());
+                if (number == -1){
+                Console.WriteLine("Thx! Bye! \n"+
+                    "Sum: "+sum+"\n"+
+                    "Numbers: "+i+"\n"+
+                    "Average: "+Convert.ToDouble((sum*1.0)/i)+"\n"+
+                    "Even: "+even+"\n"+
+                    "Odd: "+odd);
+                break;
+                } else {
+                    if (number%2 == 0) {
+                    even++;
+                    } else {
+                    odd++;
+                    }
+                sum = sum + number;
+                i++;
+
+                }
+
+                    
+
+            }
+        Console.ReadKey();
     }
   }
 }
