582d6dd8e83090be198e5765b6d34dd82d1bef27
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-16 13:03:24 +0300

    16.10.2020

--- Exercises/Part 2/Exercise 042/Program.cs
+++ Exercises/Part 2/Exercise 042/Program.cs
@@ -8,7 +8,10 @@ namespace exercise_42
     {
 
       // Write your code here:
-    
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(Math.Sqrt(firstNumber + secondNumber));
+   
     }
   }
 }
