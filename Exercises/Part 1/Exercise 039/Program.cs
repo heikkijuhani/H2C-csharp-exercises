0dea405863a5337c4eceb5becacc72097a47f00c
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-16 12:36:42 +0300

    16.10.2020

--- Exercises/Part 1/Exercise 039/Program.cs
+++ Exercises/Part 1/Exercise 039/Program.cs
@@ -7,6 +7,25 @@ namespace exercise_39
     public static void Main(string[] args)
     {
       // Write your code here:
+
+           int sum = 0;
+
+           while (true) {
+
+            Console.WriteLine("Give a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+                if (number == 0) {
+                Console.WriteLine("Total sum of numbers: "+sum);
+                break;
+
+                } else {
+                sum = sum + number;
+                }
+          
+           }
+
+           Console.ReadKey();
     }
   }
 }
