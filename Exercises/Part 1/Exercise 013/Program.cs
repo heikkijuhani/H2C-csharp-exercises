6485cf1104aeb2989b3703ac676606515d1fbdff
heikkijuhani <57320479+heikkijuhani@users.noreply.github.com>
2020-10-16 10:07:13 +0300

    16.10.2020

--- Exercises/Part 1/Exercise 013/Program.cs
+++ Exercises/Part 1/Exercise 013/Program.cs
@@ -1,13 +1,23 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 
 namespace exercise_13
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // Write your code here:
-
+        public static void Main(string[] args)
+        {
+            // Write your code here:
+            Console.WriteLine("Give a number!");
+            double number = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("You gave "+number);
+            
+        }
     }
-  }
 }
