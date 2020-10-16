7725f17d8a2b1d6d5a7c5822cfb0fd72df705ec9
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-16 12:33:15 +0300

    16.10.2020

--- Exercises/Part 1/Exercise 038/Program.cs
+++ Exercises/Part 1/Exercise 038/Program.cs
@@ -8,6 +8,25 @@ namespace exercise_38
     {
       // Write your code here:
 
+           int i = 0;
+
+           while (true) {
+
+            Console.WriteLine("Give a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+                if (number < 0){
+                i++;
+                }
+
+                if (number == 0) {
+                Console.WriteLine("Total amount of negative numbers: "+i);
+                break;
+
+                }
+          
+           }
+
     }
   }
 }
