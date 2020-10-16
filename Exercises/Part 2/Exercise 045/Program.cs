463b20e00b70f0f9e8cca07a722c613827d72d44
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-16 17:57:39 +0300

    16.10.2020

--- Exercises/Part 2/Exercise 045/Program.cs
+++ Exercises/Part 2/Exercise 045/Program.cs
@@ -8,7 +8,13 @@ namespace exercise_45
     {
 
       // Write your code here:
+        int number = Convert.ToInt32(Console.ReadLine());
+        
+            for (int i = 0; i <= number; i++) {
+            Console.WriteLine(i);
+            }
 
+            Console.ReadKey();
     }
   }
 }
