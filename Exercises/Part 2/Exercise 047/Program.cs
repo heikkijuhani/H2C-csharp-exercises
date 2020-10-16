13ad414b4b732b95827046e6eaeef750426ec267
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-16 18:06:28 +0300

    16.10.2020

--- Exercises/Part 2/Exercise 047/Program.cs
+++ Exercises/Part 2/Exercise 047/Program.cs
@@ -8,6 +8,14 @@ namespace exercise_47
     {
 
       // Write your code here:
+      Console.WriteLine("Where to?");
+      int toNumber = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Where from?");
+      int fromNumber = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = fromNumber; i <= toNumber; i++){
+            Console.WriteLine(i);
+             }
       
     }
   }
