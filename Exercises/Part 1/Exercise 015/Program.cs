38156cd1f6d7517d53abcb88cef12ab13a741791
heikkijuhani <57320479+heikkijuhani@users.noreply.github.com>
2020-10-16 10:06:01 +0300

    16.10.2020

--- Exercises/Part 1/Exercise 015/Program.cs
+++ Exercises/Part 1/Exercise 015/Program.cs
@@ -1,14 +1,40 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 
 namespace exercise_15
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
+            // Write your code here:
+            Console.WriteLine("Give a string:");
+            string word = Console.ReadLine();
+            
+            Console.WriteLine("Give a integer:");
+            string answer = Console.ReadLine();
+            int intNumber = System.Convert.ToInt32(answer);
 
-      // Write your code here:
+            Console.WriteLine("Give a double:");
+            answer = Console.ReadLine();
+            double doubleNumber = System.Convert.ToDouble(answer);
 
+            Console.WriteLine("Give a boolean:");
+            answer = Console.ReadLine();
+            bool booleanValue = System.Convert.ToBoolean(answer);
+            
+            Console.WriteLine("Your string: " + word);
+            Console.WriteLine("Your integer: " + intNumber);
+            Console.WriteLine("Your double: " + doubleNumber);
+            Console.WriteLine("Your string: " + booleanValue);
+
+
+        }
     }
-  }
 }
