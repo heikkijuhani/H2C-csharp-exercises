48e0e0e3c185ea7b516f6ed673443f6f3243e863
heikkijuhani <57320479+heikkijuhani@users.noreply.github.com>
2020-10-16 10:04:31 +0300

    16.10.2020

--- Exercises/Part 1/Exercise 017/Program.cs
+++ Exercises/Part 1/Exercise 017/Program.cs
@@ -1,14 +1,26 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 
 namespace exercise_17
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
 
-      // Write your code here:
-
+            // Write your code here:
+            Console.WriteLine("Give the first number!");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give the second number!");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            int sum = firstNumber + secondNumber;
+            Console.WriteLine("The sum is "+sum);
+        }
     }
-  }
 }
