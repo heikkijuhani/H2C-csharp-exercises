4020fba98e9430411b25330423987dcc70cae073
heikkijuhani <57320479+heikkijuhani@users.noreply.github.com>
2020-10-16 10:05:25 +0300

    16.10.2020

--- Exercises/Part 1/Exercise 016/Program.cs
+++ Exercises/Part 1/Exercise 016/Program.cs
@@ -1,14 +1,24 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 
 namespace exercise_16
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
 
-      // Write your code here:
+            // Write your code here:
+            Console.WriteLine("How many days?");
+            int days = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(days * 86400);
 
+        }
     }
-  }
 }
