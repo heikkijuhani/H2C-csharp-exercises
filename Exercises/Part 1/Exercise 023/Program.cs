7b64cfe33c7fd39d604618c25dc64073ee2a6a50
heikkijuhani <57320479+heikkijuhani@users.noreply.github.com>
2020-10-16 10:13:19 +0300

    16.10.2020

--- Exercises/Part 1/Exercise 023/Program.cs
+++ Exercises/Part 1/Exercise 023/Program.cs
@@ -1,14 +1,31 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace exercise_23
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        static void Main(string[] args)
+        {
+            // Write your code here:
+            int i = 0;
+            Console.WriteLine("Give the first number!");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+           
+            Console.WriteLine("Give the second number!");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+           
+            double firstDouble = Convert.ToDouble(firstNumber);
+            double secondDouble = Convert.ToDouble(secondNumber);
+            
+            Console.WriteLine(firstNumber+ " + " + secondNumber + " = "+ firstNumber+secondNumber);
+            Console.WriteLine(firstNumber+ " - " + secondNumber + " = "+ (firstNumber-secondNumber));
+            Console.WriteLine(firstNumber + " * " + secondNumber + " = " + (firstNumber * secondNumber));
+            Console.WriteLine(firstDouble + " / " + secondDouble + " = " + (firstDouble / secondDouble));
 
-      // Write your code here:
-
+        }
     }
-  }
 }
