5c5091690a21cff13b7e7d93484d1ad3813c20d8
heikkijuhani <57320479+heikkijuhani@users.noreply.github.com>
2020-10-16 10:11:23 +0300

    16.10.2020

--- Exercises/Part 1/Exercise 027/Program.cs
+++ Exercises/Part 1/Exercise 027/Program.cs
@@ -1,13 +1,30 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace exercise_27
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // Write your code here:
+        static void Main(string[] args)
+        {
+            // Write your code here:
+            
+            Console.WriteLine("Give a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
 
+            if (number > 0)
+            {
+                Console.WriteLine("It is positive");
+            } else
+            {
+                Console.WriteLine("It is not positive");
+            }
+
+
+
+        }
     }
-  }
 }
