1814d06be3b7f9db6fa326699011f57f39c83bf7
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-17 10:57:00 +0300

    17.10.2020

--- Exercises/Part 2/Exercise 051/Program.cs
+++ Exercises/Part 2/Exercise 051/Program.cs
@@ -7,11 +7,16 @@ namespace exercise_51
     public static void Main(String[] args)
     {
       // Call your method here:
-
-
+      PrintUntilNumber(5);
+     
     }
 
     // Write your method here:
+    public static void PrintUntilNumber(int number){
+        for (int i=1; i <= number; i++) {
+            Console.WriteLine(i);
+            }
+        }
 
   }
 }
