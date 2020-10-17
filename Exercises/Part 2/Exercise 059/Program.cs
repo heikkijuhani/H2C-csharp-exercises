2c93d5be20d4063b52b68e9909acee6df7cf638b
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-17 17:05:04 +0300

    17.10.2020

--- Exercises/Part 2/Exercise 059/Program.cs
+++ Exercises/Part 2/Exercise 059/Program.cs
@@ -6,11 +6,30 @@ namespace exercise_59
   {
     public static void Main(String[] args)
     {
-      int answer = Greatest(2, 7, 3);
+      int answer = GrEatest(2, 7, 3);
       Console.WriteLine("Greatest: " + answer);
+            Console.ReadKey();
+
     }
 
     // Write your method here:
+    public static int GrEatest(int number1, int number2, int number3) 
+      {
+  // write your code here
+  // do not print anything inside the method
+                    if (number1 > number2 && number1 > number3)  {
+                return number1;
+            } else if (number2 > number1 && number2 > number3) {
+                return number2;
+            } else {
+                return number3;
+            }
+     
+
+  // there must be a return command at the end
+    }
+
+        
 
   }
 }
