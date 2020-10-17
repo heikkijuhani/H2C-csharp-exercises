b4b5170403f2d418824d296558e917c821cd1b84
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-17 11:33:02 +0300

    17.10.2020

--- Exercises/Part 2/Exercise 058/Program.cs
+++ Exercises/Part 2/Exercise 058/Program.cs
@@ -8,9 +8,17 @@ namespace exercise_58
     {
       int answer = Smallest(2, 7);
       Console.WriteLine("Smallest: " + answer);
+            Console.ReadKey();
     }
 
     // Write your method here:
-    
+    public static int Smallest(int number1, int number2) {
+            if(number1 < number2){
+            return number1;
+            } else {
+            return number2;
+            }
+        
+    }
   }
 }
