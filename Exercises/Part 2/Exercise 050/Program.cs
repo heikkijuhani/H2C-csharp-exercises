81dc38ef8418d10f826315f0f99d545d709c029f
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-17 10:51:35 +0300

    17.10.2020

--- Exercises/Part 2/Exercise 050/Program.cs
+++ Exercises/Part 2/Exercise 050/Program.cs
@@ -7,11 +7,19 @@ namespace exercise_50
     public static void Main(string[] args)
     {
       // Call your method here:
- 
+        Console.WriteLine("How many times?");
+        int times = Convert.ToInt32(Console.ReadLine());
+        PrintPhrase(times);
+
     }
 
     // Write your method here:
-    public static void PrintPhrase() {
+    public static void PrintPhrase(int times) {
+            for (int i = 0; i < times; i++){
+                Console.WriteLine("In a hole in the ground there lived a method");
+            }
+            
+
 
     }
   }
