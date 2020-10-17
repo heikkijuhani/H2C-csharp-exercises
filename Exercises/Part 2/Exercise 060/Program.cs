fc87a0e0e8b0cf89070ee70c90b39586d9ad47ea
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-17 17:30:28 +0300

    17.10.2020

--- Exercises/Part 2/Exercise 060/Program.cs
+++ Exercises/Part 2/Exercise 060/Program.cs
@@ -6,12 +6,27 @@ namespace exercise_60
   {
         public static void Main(String[] args)
     {
+              PrintStars(5);
+              PrintSquare(4);
+              PrintRectangle(17, 3);
+              PrintTriangle(4);
+
+
+
+            Console.ReadKey();
 
     }
 
 
     public static void PrintStars(int number)
     {
+            for(int i = 0; i <= number; i++){
+            Console.Write("*");
+
+            }
+
+            Console.WriteLine("");
+                      
       // you can print one star with the command
       // Console.Write("*");
       // call the print command n times
@@ -21,16 +36,36 @@ namespace exercise_60
 
     public static void PrintSquare(int size)
     {
+            for (int i = 0; i <= size; i++){
+                for(int j = 0; j <=size; j++){
+                Console.Write("*");
+                }
+                Console.WriteLine("");
+                            
+            }
 
     }
 
     public static void PrintRectangle(int width, int height)
     {
+            for (int i = 0; i <= height; i++){
+                for(int j = 0; j <=width; j++){
+                Console.Write("*");
+                }
+                Console.WriteLine("");
+                            
+            }
 
     }
 
     public static void PrintTriangle(int size)
     {
+        for (int i = 1; i <= size; i++) {  
+            for (int j = 1; j <= i; j++) {  
+               Console.Write("*");  
+            }  
+            Console.WriteLine("");  
+         }  
 
     }
   }
