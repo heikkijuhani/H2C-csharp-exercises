c9df6832943c5a8d4dd0c34c14da414a4ff5cf40
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-18 15:56:01 +0300

    18.10.2020

--- Exercises/Part 3/Exercise 069/Program.cs
+++ Exercises/Part 3/Exercise 069/Program.cs
@@ -13,6 +13,19 @@ namespace exercise_69
         int input = Convert.ToInt32(Console.ReadLine());
         if (input == -1)
         {
+          Console.WriteLine("From where?");
+          int from = Convert.ToInt32(Console.ReadLine());
+
+          Console.WriteLine("Where to?");
+          int to = Convert.ToInt32(Console.ReadLine());
+
+          for (int i = 0; i < list.Count; i++){
+                    if (list[i] >= from && list[i] <= to){
+                        Console.WriteLine(list[i]);
+                                }
+
+                    }
+
           break;
         }
         list.Add(input);
