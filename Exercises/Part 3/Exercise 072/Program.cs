bd9ba7b6d287eb46d19e091dac96ae8cd3496e25
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-18 17:01:02 +0300

    18.10.2020

--- Exercises/Part 3/Exercise 072/Program.cs
+++ Exercises/Part 3/Exercise 072/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace exercise_72
 {
@@ -7,6 +8,23 @@ namespace exercise_72
   {
     public static void Main(string[] args)
     {
+      List<int> list = new List<int>();
+      while (true)
+      {
+        int input = Convert.ToInt32(Console.ReadLine());
+        if (input == 9999)
+        {
+          Console.WriteLine("Smallest number: "+list.Min());
+                  for (int i = 0; i < list.Count; i++){
+                    if (list[i] == list.Min()){
+                        Console.WriteLine("Found at index: "+i);
+                                }}
+
+          break;
+        }
+        list.Add(input);
+      }
+
 
     }
   }
