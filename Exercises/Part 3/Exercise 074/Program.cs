042e52d4abd17f2d0515f531df12164e236d2121
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-20 15:13:58 +0300

    20.10.2020

--- Exercises/Part 3/Exercise 074/Program.cs
+++ Exercises/Part 3/Exercise 074/Program.cs
@@ -17,8 +17,27 @@ namespace exercise_74
         }
         list.Add(input);
       }
-      
+     
+      Console.WriteLine("Search for?");           
+      string name = Console.ReadLine();
+      bool found = true;
+
+            for (int i = 0; i < list.Count; i++){
+            if (name.Equals(list[i])) {
+                found = true;
+             } else {
+                found = false;
+                 }
+            }
+        if(found == true){
+            Console.WriteLine(name+" was found!");
+        } else {
+            Console.WriteLine(name+" was not found!");
+            }
+        Console.ReadKey();        
+     }   
     }
-  }
-}
+    }
+      
+
 
