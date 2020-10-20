68aa35b6cbff93b073d4f7085a681fc4d4732f0a
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-20 20:15:57 +0300

    20.10.2020

--- Exercises/Part 3/Exercise 075/Program.cs
+++ Exercises/Part 3/Exercise 075/Program.cs
@@ -13,14 +13,34 @@ namespace exercise_75
       // List<int> numbers = new List<int>();
       // numbers.Add(5);
 
+        List<int> numbers = new List<int>();
+        numbers.Add(3);
+        numbers.Add(2);
+        numbers.Add(6);
+        numbers.Add(-1);
+        numbers.Add(5);
+        numbers.Add(1);
+
       Console.WriteLine("The numbers in the range [0, 5]");
       PrintNumbersInRange(numbers, 0, 5);
 
       Console.WriteLine("The numbers in the range [3, 10]");
       PrintNumbersInRange(numbers, 3, 10);
+    
     }
 
+    public static void PrintNumbersInRange(List<int> numbers, int lowerLimit, int upperLimit){
+        for (int i = 0; i < numbers.Count; i++){
+            if (numbers[i] >= lowerLimit && numbers[i] <= upperLimit){
+                Console.WriteLine(numbers[i]);
+                }
+        }
+    
+    
+    
+
     }
+        
   }
 }
 
