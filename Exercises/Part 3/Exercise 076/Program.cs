ee777070c50fd14ed31ea311a104ea93752f12c2
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-20 20:18:58 +0300

    20.10.2020

--- Exercises/Part 3/Exercise 076/Program.cs
+++ Exercises/Part 3/Exercise 076/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace exercise_76
 {
@@ -7,8 +8,23 @@ namespace exercise_76
   {
     public static void Main(string[] args)
     {
+            List<int> numbers = new List<int>();
+            numbers.Add(3);
+            numbers.Add(2);
+            numbers.Add(6);
+            numbers.Add(-1);
+            Console.WriteLine(Sum(numbers));
+
+            numbers.Add(5);
+            numbers.Add(1);
+            Console.WriteLine(Sum(numbers));
+
+            Console.ReadKey();
 
     }
+        public static int Sum(List<int> numbers){
+        return numbers.Sum();
+        }
 
   }
 }
