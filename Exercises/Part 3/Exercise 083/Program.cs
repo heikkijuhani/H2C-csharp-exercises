a97f8bebe8cfa2c11802a2cea1a342111371575b
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-21 20:38:26 +0300

    21.10.2020

--- Exercises/Part 3/Exercise 083/Program.cs
+++ Exercises/Part 3/Exercise 083/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace exercise_83
 {
@@ -7,8 +8,12 @@ namespace exercise_83
   {
     public static void Main(string[] args)
     {
-
-
+        Console.Write("Give a word: ");
+        string wordThreeTimes = String.Concat(Enumerable.Repeat(Console.ReadLine(),3));
+        
+        Console.WriteLine(wordThreeTimes);
+         
+            Console.ReadKey();
     }
 
   }
