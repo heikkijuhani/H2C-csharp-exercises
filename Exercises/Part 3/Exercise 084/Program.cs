e7e83f3607766aa7c15b97d67cce5b534a27c92e
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-22 15:30:25 +0300

    22.10.2020

--- Exercises/Part 3/Exercise 084/Program.cs
+++ Exercises/Part 3/Exercise 084/Program.cs
@@ -7,8 +7,14 @@ namespace exercise_84
   {
     public static void Main(string[] args)
     {
+     Console.Write("Give a string: ");
+     if (string.Equals(Console.ReadLine(), "true")){
+            Console.WriteLine("You got it right!");
+            } else {
+            Console.WriteLine("Try again!");
+            }
 
-
+     Console.ReadKey();
     }
   }
 }
