beef816e9cecb4381117b24bf76c285da5816424
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-23 20:44:18 +0300

    23.10.2020

--- Exercises/Part 3/Exercise 087/Program.cs
+++ Exercises/Part 3/Exercise 087/Program.cs
@@ -8,9 +8,25 @@ namespace exercise_87
     public static void Main(string[] args)
     {
 
+      while (true){
+            string input = Console.ReadLine();
+              if(input != ""){
+                string[] words = input.Split( );
+                        for(int i = 0; i < words.Length; i++){
+                            if (words[i].Contains("av")) {
+                                Console.WriteLine(words[i]);
+                        }
+                        
+                    }
+              } else {
+                break;
+                }
+
+            }
+
+
+
     }
-
   }
-
 }
 
