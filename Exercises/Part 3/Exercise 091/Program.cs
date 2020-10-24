e4bf10b5ab0e5fd96b9a3cdfb9d1453e1dc96d10
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-24 10:30:39 +0300

    24.10.2020

--- Exercises/Part 3/Exercise 091/Program.cs
+++ Exercises/Part 3/Exercise 091/Program.cs
@@ -7,10 +7,28 @@ namespace exercise_91
   {
     public static void Main(string[] args)
     {
+      int bigestAge = 0;
+      string oldestName = "";
+      while (true){
+            
+            string input = Console.ReadLine();
+              if(input != ""){
+                string[] nameAndAge = input.Split(',');
+               
+                int age = Convert.ToInt32(nameAndAge[1]);
+                if (bigestAge < age){
+                    oldestName = nameAndAge[0];
+                            }
+                                
+              } else {
+                Console.WriteLine("Name of the oldest: "+oldestName);
+                break;
+                }
+
+            }
+
+      Console.ReadKey();
 
     }
   }
 }
-
-
-
