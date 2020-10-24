cefbdf39aafe790fdd0299cd959379978929cc9c
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-24 10:43:46 +0300

    24.10.2020

--- Exercises/Part 3/Exercise 092/Program.cs
+++ Exercises/Part 3/Exercise 092/Program.cs
@@ -7,10 +7,35 @@ namespace exercise_92
   {
     public static void Main(string[] args)
     {
+      int born;
+      int age;
+      int bigestAge = 0;
+      string longestName = "";
+      string name;
+      while (true){
+            
+            string input = Console.ReadLine();
+              if(input != ""){
+                string[] nameAndBorn = input.Split(',');
+                name = nameAndBorn[0];
+                born = Convert.ToInt32(nameAndBorn[1]);
+                age = 2020 - born;
 
+                if (bigestAge < age){
+                    bigestAge = age;
+                            }
+                if (longestName.Length < name.Length){
+                    longestName = name;
+                            }
+
+                                
+              } else {
+                Console.WriteLine("Longest name: "+longestName);
+                Console.WriteLine("Highest age: "+bigestAge);
+                break;
+                }
+
+            }
     }
   }
 }
-
-
-
