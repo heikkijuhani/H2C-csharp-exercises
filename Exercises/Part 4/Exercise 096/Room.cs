262cb11ed0baf7a8a36a2a3832e84d6e1e3ce466
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-26 18:09:43 +0200

    26.10.2020

---
+++ Exercises/Part 4/Exercise 096/Room.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_096
+{
+    class Room
+    {
+        private string code;
+        private int sEats;
+
+        public Room(string classCode, int numberOfSEats)
+        {
+            this.code = classCode;
+            this.sEats = numberOfSEats;
+
+
+        }
+    }
+}
