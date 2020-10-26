a37b0b55058839dc8ea9141ee0d69f68f5fb59e7
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-26 17:59:01 +0200

    26.10.2020

--- Exercises/Part 4/Exercise 094/Program.cs
+++ Exercises/Part 4/Exercise 094/Program.cs
@@ -9,7 +9,17 @@ namespace exercise_94
       // DO NOT TOUCH THE OTHER FILE!
       // Do your code here!
 
-    }
+             Account heikkiKumparesAccount = new Account("Heikki Kumpare's account", 1000.00);
+             Account personalAccount = new Account("Personal account", 00.00);
+
+            heikkiKumparesAccount.Withdrawal(100.0);
+            personalAccount.Deposit(100.0);
+
+            Console.WriteLine(heikkiKumparesAccount);
+            Console.WriteLine(personalAccount);
+
+
+        }
   }
 }
 
