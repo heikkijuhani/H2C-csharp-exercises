dae48fc92421b7832cadf0a71de14ac1d33fb6a2
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-26 18:18:38 +0200

    26.10.2020

---
+++ Exercises/Part 4/Exercise 097/Whistle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercise_97
+{
+    internal class Whistle
+    {
+        private string sound;
+
+        public Whistle(string whistleSound)
+        {
+            this.sound = whistleSound;
+        }
+
+        public void Sound()
+        {
+            Console.WriteLine(sound);
+        }
+
+    }
+}
