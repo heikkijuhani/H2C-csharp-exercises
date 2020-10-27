af8ec025b1023130fc86aa85af680dc0ee065853
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-27 14:50:00 +0200

    27.10.2020

---
+++ Exercises/Part 4/Exercise 101/Dalmatian.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exercise_101;
+
+namespace Exercise_101
+{
+    public class Dalmatian
+    {
+        public string name { get; set; }
+        public int spots { get; set; }
+
+        public Dalmatian(string name, int spots)
+        {
+            this.name = name;
+            this.spots = spots;
+        }
+    }
+}
--- Exercises/Part 4/Exercise 101/Program.cs
+++ Exercises/Part 4/Exercise 101/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Exercise_101;
 
 namespace exercise_101
 {
@@ -8,6 +9,10 @@ namespace exercise_101
     {
       Dalmatian spotty = new Dalmatian("Spot", 306);
       Console.WriteLine(spotty.name + " is a very good dog. He has " + spotty.spots + " darker spots in his fur");
+
+    Dalmatian doggo = new Dalmatian("Doggo", 999);
+    Console.WriteLine(doggo.name + " is a very good dog. He has " + doggo.spots + " darker spots in his fur");
+
     }
   }
 }
