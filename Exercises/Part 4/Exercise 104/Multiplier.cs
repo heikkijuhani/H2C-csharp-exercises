610e2341067ce47d5c46c53915f1b6219abe9e43
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-27 15:36:42 +0200

    27.10.2020

---
+++ Exercises/Part 4/Exercise 104/Multiplier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_104
+{
+    class Multiplier
+    {
+        int number { get; set; }
+
+        public Multiplier(int number)
+        {
+            this.number = number;
+                        
+        }
+
+        public int Multiply(int number)
+        {
+            int instanceValue = this.number * number;
+            this.number = instanceValue;
+            return instanceValue;
+            
+                       
+            
+        }
+    }
+}
--- Exercises/Part 4/Exercise 104/Program.cs
+++ Exercises/Part 4/Exercise 104/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Exercise_104;
 
 namespace exercise_104
 {
@@ -6,15 +7,18 @@ namespace exercise_104
   {
     public static void Main(string[] args)
     {
-      Multiplier multiplyByThree = new Multiplier(3);
+            Multiplier multiplyByThree = new Multiplier(3);
 
-      Console.WriteLine("multiplyByThree.Multiply(2): " + multiplyByThree.Multiply(2));
+            Console.WriteLine("multiplyByThree.Multiply(2): " + multiplyByThree.Multiply(2));
 
-      Multiplier multiplyByFour = new Multiplier(4);
+            Multiplier multiplyByFour = new Multiplier(4);
 
-      Console.WriteLine("multiplyByFour.Multiply(2): " + multiplyByFour.Multiply(2));
-      Console.WriteLine("multiplyByThree.Multiply(1): " + multiplyByThree.Multiply(1));
-      Console.WriteLine("multiplyByFour.Multiply(1): " + multiplyByFour.Multiply(1));
+            Console.WriteLine("multiplyByFour.Multiply(2): " + multiplyByFour.Multiply(2));
+            Console.WriteLine("multiplyByThree.Multiply(1): " + multiplyByThree.Multiply(1));
+            Console.WriteLine("multiplyByFour.Multiply(1): " + multiplyByFour.Multiply(1));
+            Console.WriteLine("multiplyByFour.Multiply(3): " + multiplyByFour.Multiply(3));
+
+            Console.ReadKey();
     }
   }
 }
