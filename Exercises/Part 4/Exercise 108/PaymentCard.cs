3d8a14bb8fa09ff0580023275e7f8e565d0d8748
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-27 19:20:20 +0200

    27.10.2020

---
+++ Exercises/Part 4/Exercise 108/PaymentCard.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercise_108
+{
+    internal class PaymentCard
+    {
+        internal double balance;
+
+        public PaymentCard(double openingBalance)
+        {
+            // write code here
+            this.balance = openingBalance;
+        }
+
+        public void EatLunch()
+        {
+            if (balance >= 10.60) { 
+            balance = balance - 10.60;
+            }
+        }
+
+        public void DrinkCoffee()
+        {
+            if(balance >= 2.00) { 
+            balance = balance - 2.00;
+            }
+        }
+
+        public override string ToString()
+        {
+            // write code here
+            return $"The card has a balance of {balance} euros";
+        }
+    }
+}
--- Exercises/Part 4/Exercise 108/Program.cs
+++ Exercises/Part 4/Exercise 108/Program.cs
@@ -1,23 +1,22 @@
-﻿using System;
-
-namespace exercise_108
+﻿namespace exercise_108
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    using Exercise_108;
+    using System;
+
+    internal class Program
     {
-      PaymentCard card = new PaymentCard(10);
-      Console.WriteLine(card);
+        public static void Main(string[] args)
+        {
+            PaymentCard card = new PaymentCard(10);
+            Console.WriteLine(card);
 
-      card.EatLunch();
-      Console.WriteLine(card);
+            card.EatLunch();
+            Console.WriteLine(card);
 
-      card.DrinkCoffee();
-      Console.WriteLine(card);
+            card.DrinkCoffee();
+            Console.WriteLine(card);
+
+            Console.ReadKey();
+        }
     }
-  }
 }
-
-
-
-
