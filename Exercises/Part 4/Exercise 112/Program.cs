e1a39474987bd445d8e0264fdba2d1703db5afca
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-30 09:34:02 +0200

    30.10.2020

--- Exercises/Part 4/Exercise 112/Program.cs
+++ Exercises/Part 4/Exercise 112/Program.cs
@@ -7,8 +7,36 @@ namespace exercise_112
   {
     public static void Main(string[] args)
     {
-      // IMPLEMENT YOUR CODE IN HERE!
+            // IMPLEMENT YOUR CODE IN HERE!
+            List<TelevisionProgram> televisionPrograms = new List<TelevisionProgram>();
 
+            while (true)
+            {
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                if(name == "")
+                {
+                    Console.WriteLine("Program's maximum duration? ");
+                    int maxDuration = Convert.ToInt32(Console.ReadLine());
+                    for(int i = 0; i < televisionPrograms.Count; i++)
+                    {
+                        if(televisionPrograms[i].duration <= maxDuration)
+                        {
+                            Console.WriteLine(televisionPrograms[i]);
+                        }
+                    }
+                    break;
+                }
+                Console.Write("Duration: ");
+                int duaration = Convert.ToInt32(Console.ReadLine());
+
+                TelevisionProgram program = new TelevisionProgram(name, duaration);
+                televisionPrograms.Add(program);
+
+
+            }
+
+            Console.ReadKey();
 
     }
   }
