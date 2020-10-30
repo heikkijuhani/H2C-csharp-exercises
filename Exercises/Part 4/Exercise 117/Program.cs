eb8b9c361d720a431f39968ece779f8479733571
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-30 15:19:20 +0200

    30.10.2020

--- Exercises/Part 4/Exercise 117/Program.cs
+++ Exercises/Part 4/Exercise 117/Program.cs
@@ -1,15 +1,25 @@
-﻿using System;
-using System.IO;
-
-namespace exercise_117
+﻿namespace exercise_117
 {
-  class Program
-  {
-    public static void Main(string[] args)
-    {
-      // Ask the user for the file name and print the content of the file
+    using System;
+    using System.IO;
 
-      
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Ask the user for the file name and print the content of the file
+
+            Console.WriteLine("Which file should have its contents printed?");
+            string file = Console.ReadLine();
+            string[] lines = File.ReadAllLines(file);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Console.WriteLine(lines[i]);
+            }
+
+
+            Console.ReadKey();
+        }
     }
-  }
 }
