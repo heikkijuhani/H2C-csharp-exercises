27c013ca44aa46b0361b146d1499d99fc403b1fc
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-30 10:47:26 +0200

    30.10.2020

--- Exercises/Part 4/Exercise 115/Program.cs
+++ Exercises/Part 4/Exercise 115/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
-
-namespace exercise_115
+﻿namespace exercise_115
 {
-  class Program
-  {
-    public static void Main(string[] args)
-    {
+    using System;
 
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i = 0;
+            while (true)
+            {
+                string inputString = Console.ReadLine();
+                if (inputString == "end")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(Math.Pow((Convert.ToInt32(inputString)), (3.0)));
+
+
+
+                }
+
+            }
+        }
     }
-  }
 }
