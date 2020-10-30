774498a01ddc4cdfc534ffa913a0df40e31d399f
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-30 10:23:18 +0200

    30.10.2020

---
+++ Exercises/Part 4/Exercise 113/Book.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_113
+{
+    public class Book
+    {
+        public string title { get; set; }
+        public int pages { get; set; }
+        public int year { get; set; }
+        public string whatToPrint { get; set; }
+
+        public Book(string title, int pages, int year)
+        {
+            this.title = title;
+            this.pages = pages;
+            this.year = year;
+        }
+
+        public string Title()
+        {
+            return this.title;
+        }
+
+        public int Year()
+        {
+            return this.year;
+        }
+
+        public int Pages()
+        {
+            return this.pages;
+        }
+
+        public string ToString()
+        {
+            return title + ", " + pages + " pages, " + year;
+        }
+
+    }
+}
--- Exercises/Part 4/Exercise 113/Program.cs
+++ Exercises/Part 4/Exercise 113/Program.cs
@@ -1,19 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace exercise_113
+﻿namespace exercise_113
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    using Exercise_113;
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
     {
-      // IMPLEMENT YOUR CODE IN HERE!
-      // Remember also to create the file Book.cs and class in it!
+        public static void Main(string[] args)
+        {
+            // IMPLEMENT YOUR CODE IN HERE!
+            // Remember also to create the file Book.cs and class in it!
 
+            List<Book> books = new List<Book>();
+
+            while (true)
+            {
+                Console.Write("Name: ");
+                string title = Console.ReadLine();
+                if (title == "")
+                {
+                    Console.WriteLine("What information will be printed?");
+                    string whatToPrint = Console.ReadLine();
+                    if (whatToPrint == "everything")
+                    {
+                        for (int i = 0; i < books.Count; i++)
+                        {
+                            Console.WriteLine(books[i].ToString());
+
+                        }
+                        break;
+                    }
+                    if (whatToPrint == "title")
+                    {
+                        for (int i = 0; i < books.Count; i++)
+                        {
+                            Console.WriteLine(books[i].Title());
+                        }
+                        break;
+                    }
+                    else { break; }
+
+                }
+
+                Console.Write("Pages: ");
+                int pages = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Publication : ");
+                int year = Convert.ToInt32(Console.ReadLine());
+
+                Book book = new Book(title, pages, year);
+                books.Add(book);
+
+
+
+
+
+            }
+            Console.ReadKey();
+        }
     }
-  }
 }
-
-
-
-
