396765a39997a3a53634811d29b71a2ee6c44e85
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-30 15:11:17 +0200

    30.10.2020

--- Exercises/Part 4/Exercise 116/Program.cs
+++ Exercises/Part 4/Exercise 116/Program.cs
@@ -7,9 +7,18 @@ namespace exercise_116
   {
     public static void Main(string[] args)
     {
-      // Read the file data.txt and print the text from it as it is
-      // You can use either File.ReadAllText or File.ReadAllLines
-      
+            // Read the file data.txt and print the text from it as it is
+            // You can use either File.ReadAllText or File.ReadAllLines
+
+            string file = "data.txt";
+            string[] lines = File.ReadAllLines(file);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Console.WriteLine(lines[i]);
+            }
+
+            Console.ReadKey();
     }
   }
 }
