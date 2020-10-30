0422d2ad2077cad54b30bb69e1de4693d5c2470d
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-10-30 09:04:32 +0200

    30.10.2020

--- Exercises/Part 4/Exercise 110/Program.cs
+++ Exercises/Part 4/Exercise 110/Program.cs
@@ -7,7 +7,30 @@ namespace exercise_110
   {
     public static void Main(string[] args)
     {
-      // IMPLEMENT YOUR CODE IN HERE!
+            // IMPLEMENT YOUR CODE IN HERE!
+            List<Item> items = new List<Item>();
+            
+            while (true)
+            {
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                if(name != "")
+                {
+                    Item item = new Item(name);
+                    items.Add(item);
+                }
+                if(name == "")
+                {
+                    for (int i = 0; i < items.Count; i++)
+                    {
+                        Console.WriteLine(items[i]);
+                    }
+                    break;
+                }
+            }
+
+            Console.ReadKey();
     }
   }
 }
