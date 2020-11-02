f7eedd93932281f34cfdfbecfe42a3f2883678cc
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-11-02 18:54:19 +0200

    02.11.2020

---
+++ Exercises/Part 5/Exercise 123/Counter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exercise_123;
+
+namespace Exercise_123
+{
+    class Counter
+    {
+        public int value { get; set; }
+
+        public Counter(int startValue)
+        {
+            this.value = startValue;
+        }
+
+        public Counter()
+        {
+            this.value = 0;
+        }
+
+        public void Increase()
+        {
+            this.value++;
+        }
+
+        public void Decrease()
+        {
+            this.value--;
+        }
+
+        public void Increase(int increaseBy)
+        {
+            if(increaseBy > 0)
+            {
+                this.value = this.value + increaseBy;
+            }
+        }
+
+        public void Decrease(int decreaseBy)
+        {
+            if(decreaseBy > 0)
+            {
+                this.value = this.value - decreaseBy;
+            }
+        }
+
+
+    }
+}
--- Exercises/Part 5/Exercise 123/Program.cs
+++ Exercises/Part 5/Exercise 123/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Exercise_123;
 
 namespace exercise_123
 {
@@ -8,7 +9,32 @@ namespace exercise_123
     {
       // You can test your code here.
       Counter counter = new Counter();
-      
-    }
+
+            counter.Increase();
+            counter.Increase();
+            counter.Increase();
+            counter.Increase();
+            counter.Increase();
+            Console.WriteLine(counter.value); //5
+            counter.Increase(10);
+            Console.WriteLine(counter.value); //15
+            counter.Increase(-10);
+            Console.WriteLine(counter.value); //15
+
+            counter.Decrease();
+            counter.Decrease();
+            counter.Decrease();
+            counter.Decrease();
+            counter.Decrease();
+
+            Console.WriteLine(counter.value); //10
+            counter.Decrease(5);
+            Console.WriteLine(counter.value); //5
+            counter.Decrease(-5);
+            Console.WriteLine(counter.value); //5
+
+            Console.ReadLine();
+
+        }
   }
 }
