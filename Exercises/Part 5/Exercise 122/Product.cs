a4a09d2efb7810ab7fa8be5c59f3d2f3aac0855c
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-11-02 18:44:41 +0200

    02.11.2020

--- Exercises/Part 5/Exercise 122/Product.cs
+++ Exercises/Part 5/Exercise 122/Product.cs
@@ -7,14 +7,33 @@ namespace exercise_122
     private string location;
     private int weight;
 
-    // CREATE YOUR CONSTRUCTORS HERE
+        // CREATE YOUR CONSTRUCTORS HERE
 
+        public Product(string name)
+        {
+            this.name = name;
+            this.location = "self";
+            this.weight = 1;
+        }
 
+        public Product(string name, string location)
+        {
+            this.location = location;
+            this.name = name;
+            this.weight = 1;
+        }
+
+        public Product(string name, int weight)
+        {
+            this.name = name;
+            this.weight = weight;
+            this.location = "warehouse";
+        }
 
 
 
     // DO NOT TOUCH THIS
-    public override string ToString()
+        public override string ToString()
     {
       return $"{this.name} ({this.weight} kg) can be found from the {this.location}.";
     }
--- Exercises/Part 5/Exercise 122/Program.cs
+++ Exercises/Part 5/Exercise 122/Program.cs
@@ -14,6 +14,8 @@ namespace exercise_122
       Console.WriteLine(tapeMeasure);
       Console.WriteLine(plaster);
       Console.WriteLine(tyre);
+
+            Console.ReadKey();
     }
   }
 }
