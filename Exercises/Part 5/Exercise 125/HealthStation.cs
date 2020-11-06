d957e8c24b09873424eba25f922f0c0deaeeb08d
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-11-06 17:53:13 +0200

    06.11.2020

--- Exercises/Part 5/Exercise 125/HealthStation.cs
+++ Exercises/Part 5/Exercise 125/HealthStation.cs
@@ -4,18 +4,28 @@ namespace exercise_125
   {
     public int weighings { get; private set; }
 
-    // Create a constructor here, if needed
+        // Create a constructor here, if needed
+        public HealthStation()
+        {
+            this.weighings = 0;
+                
+        }
 
     public int Weigh(Person person)
     {
-      // return the weight of the person passed as the parameter
-      // incrase weighings
-      return 0;
-    }
+            // return the weight of the person passed as the parameter
+            // incrase weighings
+            weighings++;
+            return person.weight;
+            
+            
+        }
 
     public void Feed(Person person)
     {
-      // Increse the weight of the person with one kilogram
+            // Increse the weight of the person with one kilogram
+            person.weight = person.weight + 1;
+
     }
   }
 }
