910cc07d62f5225d519185cf420ede3827524476
Heikki Kumpare <heikki.kumpare@centria.fi>
2020-11-14 11:53:03 +0200

    14.11.2020

--- Exercises/Part 6/Exercise 137/Person.cs
+++ Exercises/Part 6/Exercise 137/Person.cs
@@ -1,35 +1,44 @@
 namespace exercise_136
 {
-  public class Person
-  {
-    private string name;
-    private int age;
+    using System;
 
-    public Person(string givenName)
+    public class Person
     {
-      this.name = givenName;
-      this.age = 0;
-    }
+        private string name;
 
-    public bool IsOfAge()
-    {
-      return (this.age >= 18);
-    }
+        private int age;
 
-    public int HowManyNames()
-    {
-      // Do something here
-      return 0;
-    }
+        public Person(string givenName)
+        {
+            this.name = givenName;
+            this.age = 0;
+        }
 
-    public void GrowOlder()
-    {
-      this.age++;
-    }
+        public bool IsOfAge()
+        {
+            return (this.age >= 18);
+        }
 
-    public override string ToString()
-    {
-      return this.name;
+        public int HowManyNames()
+        {
+            // Do something here
+            string personNames = this.ToString();
+            var namesCount = personNames.Split(' ').Length;
+            int numberOfNames = Convert.ToInt32(namesCount);
+
+
+
+            return numberOfNames;
+        }
+
+        public void GrowOlder()
+        {
+            this.age++;
+        }
+
+        public override string ToString()
+        {
+            return this.name;
+        }
     }
-  }
 }
